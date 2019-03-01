2b343227019bd08c30715223a9cd590e06bdef75
Myrecord <Myrecord@users.noreply.github.com>
2019-03-01 16:16:06 +0800

    Update config.py

--- config.py
+++ config.py
@@ -9,16 +9,16 @@ class Config:
     FLASKY_ADMIN = '' #管理员邮件地址
     DEBUG = True
     MAIL_SERVER = '' #邮件服务器
-    MAIL_PORT = 465
-    MAIL_USE_TLS = False
-    MAIL_USE_SSL = True
+    MAIL_PORT = 465  #邮件端口
+    MAIL_USE_TLS = False 
+    MAIL_USE_SSL = True 
     MAIL_USERNAME = '' #邮件账户
     MAIL_PASSWORD = ''		#邮件密码
-    SQLALCHEMY_DATABASE_URI = 'mysql://root:@127.0.0.1/myweb'
+    SQLALCHEMY_DATABASE_URI = 'mysql://root:@127.0.0.1/infomanage' #mysql数据库地址
     SCHEDULER_API_ENABLED = True
     use_reloader=False
-    UPLOADED_FILE_DEST = '/Users/root1/webdev/fileloads'
-    SCRIPT_LOCAL_PATH = '/Users/root1/webdev/script'
+    UPLOADED_FILE_DEST = '/Users/root1/infomanage/fileloads'
+    SCRIPT_LOCAL_PATH = '/Users/root1/infomanage/script'
 
     @staticmethod
     def init_app(app):
