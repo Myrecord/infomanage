ff6e09f0d49abf03f806e175265b54246037178c
Myrecord <Myrecord@users.noreply.github.com>
2019-03-06 12:18:02 +0800

    update rdsdata.py

--- script/rdsdata.py
+++ script/rdsdata.py
@@ -11,9 +11,10 @@ class datainfo():
 		self.types = ['sharding', 'replicate']
 		self.DBid = []
 		self.DBinfo = []
+		self.redis = []
+		self.mogo = []
 
-
-	def get_DBid(self):
+	def query_mysql(self):
 		try:
 			for zone in current_app.config['ALIYUN_ZONE']:
 				client = AcsClient(current_app.config['ALIYUN_ACCESS_KEYID'], current_app.config['ALIYUN_ACCESS_KEY_SECRET'], zone)
@@ -23,9 +24,6 @@ class datainfo():
 				self.DBid.append(response['Items']['DBInstance'])
 		except Exception as e:
 			raise e
-
-	def query_mysql(self):
-		self.get_DBid()
 		for zone in current_app.config['ALIYUN_ZONE']:
 			client = AcsClient(current_app.config['ALIYUN_ACCESS_KEYID'], current_app.config['ALIYUN_ACCESS_KEY_SECRET'],zone)
 			for ids in sum(self.DBid, []):
@@ -35,30 +33,30 @@ class datainfo():
 				response = json.loads(client.do_action_with_exception(request))
 				self.DBinfo.append(response['Items']['DBInstanceAttribute'])
 			return self.DBinfo
-
-	def query_redis(self):
-		for zone in current_app.config['ALIYUN_ZONE']:
-			client  = AcsClientv3(current_app.config['ALIYUN_ACCESS_KEYID'], current_app.config['ALIYUN_ACCESS_KEY_SECRET'],zone)
-			request = CommonRequest()
-			request.set_accept_format('json')
-			request.set_domain('r-kvstore.aliyuncs.com')
-			request.set_action_name('DescribeInstances')
-			request.set_version('2015-01-01')
-			response = json.loads(client.do_action_with_exception(request))
-			self.DBinfo.append(response['Instances']['KVStoreInstance'])
-		return self.DBinfo
-
-	def query_mogo(self):
-		for zone in current_app.config['ALIYUN_ZONE']:
-			client  = AcsClientv3(current_app.config['ALIYUN_ACCESS_KEYID'], current_app.config['ALIYUN_ACCESS_KEY_SECRET'],zone)
-			for types in self.types:
-				request = CommonRequest()
-				request.set_accept_format('json')
-				request.set_domain('mongodb.aliyuncs.com')
-				request.set_action_name('DescribeDBInstances')
-				request.set_version('2015-12-01')
-				request.add_query_param('DBInstanceType',types)
-				response = json.loads(client.do_action_with_exception(request))
-				self.DBinfo.append(response['DBInstances']['DBInstance'])
-		return self.DBinfo
+	#
+	# def query_redis(self):
+	# 	for zone in current_app.config['ALIYUN_ZONE']:
+	# 		client  = AcsClientv3(current_app.config['ALIYUN_ACCESS_KEYID'], current_app.config['ALIYUN_ACCESS_KEY_SECRET'],zone)
+	# 		request = CommonRequest()
+	# 		request.set_accept_format('json')
+	# 		request.set_domain('r-kvstore.aliyuncs.com')
+	# 		request.set_action_name('DescribeInstances')
+	# 		request.set_version('2015-01-01')
+	# 		response = json.loads(client.do_action_with_exception(request))
+	# 		self.redis.append(response['Instances']['KVStoreInstance'])
+	# 	return self.DBinfo
+	#
+	# def query_mogo(self):
+	# 	for zone in current_app.config['ALIYUN_ZONE']:
+	# 		client  = AcsClientv3(current_app.config['ALIYUN_ACCESS_KEYID'], current_app.config['ALIYUN_ACCESS_KEY_SECRET'],zone)
+	# 		for types in self.types:
+	# 			request = CommonRequest()
+	# 			request.set_accept_format('json')
+	# 			request.set_domain('mongodb.aliyuncs.com')
+	# 			request.set_action_name('DescribeDBInstances')
+	# 			request.set_version('2015-12-01')
+	# 			request.add_query_param('DBInstanceType',types)
+	# 			response = json.loads(client.do_action_with_exception(request))
+	# 			self.mogo.append(response['DBInstances']['DBInstance'])
+	# 	return self.DBinfo
 
